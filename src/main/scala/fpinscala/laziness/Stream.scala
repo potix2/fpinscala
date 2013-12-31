16d0c1efc6191022cb4571957881fcd687d9767a
Katsunori Kanda <potix2@gmail.com>
2014-01-01 01:57:23 +0900

    solved exercise 4 in chapter5

--- src/main/scala/fpinscala/laziness/Stream.scala
+++ src/main/scala/fpinscala/laziness/Stream.scala
@@ -20,6 +20,21 @@ sealed abstract class Stream[+A] {
    * exercise3
    */
   def takeWhile(p: A => Boolean): Stream[A]
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a,b) => p(a) || b)
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    uncons match {
+      case Some(c) => f(c.head, c.tail.foldRight(z)(f))
+      case None => z
+    }
+
+  /**
+   * exercise4
+   */
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a,b) => p(a) && b)
 }
 
 object Empty extends Stream[Nothing] {
--- src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -21,4 +21,20 @@ class StreamSpec extends Specification {
       Stream(1,2,3,4,3,2,1).takeWhile(_ < 3).toList must_== List(1,2)
     }
   }
+  "exists" should {
+    "be true when an element matching a Boolean function exists" in {
+      Stream(1,2,3,4,5).exists(_ % 2 == 0) must beTrue
+    }
+    "be false when an element matching a Boolean function exists" in {
+      Stream(1,2,3,4,5).exists(_ > 5) must beFalse
+    }
+  }
+  "forAll" should {
+    "be true when the all elements match a given predicate" in {
+      Stream(2,4,6,8,10).forAll(_ % 2 == 0) must beTrue
+    }
+    "be false when an element not matching a Boolean function exists" in {
+      Stream(2,4,6,8,11,12).forAll(_ % 2 == 0) must beFalse
+    }
+  }
 }
